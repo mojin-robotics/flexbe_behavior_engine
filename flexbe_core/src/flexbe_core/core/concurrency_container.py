17f45e1bc5cb41614bc370daaa053df2129e57a9
Denis Lehmann <denis.lehmann@4am-robotics.com>
2026-04-22 17:47:21 +0200

    reset outcomes and current state on exit and preemption

--- flexbe_core/src/flexbe_core/core/concurrency_container.py
+++ flexbe_core/src/flexbe_core/core/concurrency_container.py
@@ -62,6 +62,8 @@ class ConcurrencyContainer(OperatableStateMachine):
         outcome = None
         if any(self._returned_outcomes[state.name] == state._preempted_name
                for state in self._states if state.name in self._returned_outcomes):
+            self._returned_outcomes = dict()
+            self._current_state = None
             return self._preempted_name  # handle preemption if required
         # check conditions
         for item in self._conditions:
@@ -119,3 +121,5 @@ class ConcurrencyContainer(OperatableStateMachine):
             if state in self._returned_outcomes:
                 continue  # skip states that already exited themselves
             self._execute_single_state(state, force_exit=True)
+        self._returned_outcomes = dict()
+        self._current_state = None
